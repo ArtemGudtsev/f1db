66017d4cd4b7a828cee38108fbbb9413453441d0
Gudtsev Artem <Artem.Gudtsev@gmail.com>
2020-06-28 21:34:23 +0200

    fixed error with default date

--- migrations/20200621_062852_create_table_races.sql
+++ migrations/20200621_062852_create_table_races.sql
@@ -6,7 +6,7 @@ CREATE TABLE "races" (
   "round" int NOT NULL DEFAULT '0',
   "circuitId" int NOT NULL DEFAULT '0',
   "name" varchar(255) NOT NULL DEFAULT '',
-  "date" date NOT NULL DEFAULT '0000-00-00',
+  "date" date NOT NULL,
   "time" time DEFAULT NULL,
   "url" varchar(255) DEFAULT NULL,
   PRIMARY KEY ("raceId")
