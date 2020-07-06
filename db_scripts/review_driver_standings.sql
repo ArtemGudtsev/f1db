b87a1c759babd1994ccd8b6ef1c426111422ff11
Gudtsev Artem <Artem.Gudtsev@gmail.com>
2020-07-06 23:45:09 +0200

    add review sql scripts for vw_DriverStandings view

---
+++ db_scripts/review_driver_standings.sql
@@ -0,0 +1,5 @@
+select * from public.vw_driverstandings
+where position in ('1', '2', '3') and year in ('2019','2018','2017','2016','2015');
+
+select * from public.vw_driverstandings
+where position in ('1', '2', '3') and year in ('2004', '2005', '2006', '2007');
