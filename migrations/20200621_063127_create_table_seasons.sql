2e09c849d0c3d3af976cfd5a976aadd3d1da5bf3
Gudtsev Artem <Artem.Gudtsev@gmail.com>
2020-07-06 23:12:39 +0200

    replace manual migrations with snapshot from current db

--- migrations/20200621_063127_create_table_seasons.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS "seasons";
-
-CREATE TABLE "seasons" (
-  "seasonId" int NOT NULL DEFAULT '0',
-  "year" int NOT NULL DEFAULT '0',
-  "url" varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY ("year")
-);
--- migrations/20200621_063412_create_table_status.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS "status";
-
-CREATE TABLE "status" (
-  "statusId" int NOT NULL,
-  "status" varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY ("statusId")
-);
